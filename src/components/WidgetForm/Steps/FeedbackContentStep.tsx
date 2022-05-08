500038643131d1adfe5eeec02812a7e26eb2df8b
Ruy Monteiro <ruystfrancis@gmail.com>
2022-05-08 03:15:57 -0300

    feat: Content Step submit logic

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,5 +1,5 @@
 import { ArrowLeft } from "phosphor-react";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import { FeedbackType } from "..";
 import { feedbackTypes } from "../../../data/feedbackTypes";
 import { CloseButton } from "../../CloseButton";
@@ -12,8 +12,18 @@ interface FeedbackContentStepProps {
 
 export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested }: FeedbackContentStepProps) {
     const [screenshot, setScreenshot] = useState<string | null>(null);
+    const [comment, setComment] = useState('');
 
     const feedbackTypeInfo = feedbackTypes[feedbackType];
+
+    function handleSubmitFeedback(event: FormEvent) {
+        event.preventDefault();
+
+        console.log({
+            screenshot,
+            comment
+        });
+    }
     
     return (
         <>
@@ -37,10 +47,12 @@ export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested }
                 <CloseButton />
             </header>
 
-            <form className="my-4 w-full">
+            <form onSubmit={handleSubmitFeedback} className="my-4 w-full">
                 <textarea 
                     className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-brand-500 focus:ring-1 resize-none focus:outline-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
                     placeholder="Conte com detalhes o que está acontecendo..."
+                    onChange={event => setComment(event.target.value)}
+                    value={comment}
                 />
 
                 <footer className="flex gap-2 mt-2">
@@ -51,7 +63,8 @@ export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested }
 
                     <button
                         type="submit"
-                        className="p-2 bg-brand-500 rounded-sm border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500/20 transition-colors"
+                        disabled={!comment}
+                        className="p-2 bg-brand-500 rounded-sm border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500/20 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500 disabled:cursor-not-allowed"
                     >
                         Enviar feedback
                     </button>
